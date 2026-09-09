673b3920563e72962dbd428769539c6fc48dd5c0
svfranklindemo <166366015+svfranklindemo@users.noreply.github.com>
2026-09-09 19:53:32 +0530

    Uncomment footer beacon functions

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -72,6 +72,6 @@ export default async function decorate(block) {
   while (fragment.firstElementChild) footer.append(fragment.firstElementChild);
 
   block.append(footer);
-  //fireAdobeFooterBeacon();
-  //fireAdobeFooterBeaconForRefDemo(siteName);
+  fireAdobeFooterBeacon();
+  fireAdobeFooterBeaconForRefDemo(siteName);
 }
